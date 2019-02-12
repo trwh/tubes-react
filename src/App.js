07450d2a2098a4b627f4f86cc1d72ef3b27176ce
Tim Harrison <12405008+trwh@users.noreply.github.com>
2019-02-12 18:01:01 +0000

    Add function to get an array of possible tube lines from the TFL API.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      station: "Richmond"
+    };
+  }
+
   render() {
+    getTubeLines();
     return (
       <div>
-        Hello!
+        Hello! Let's look for {this.state.station}!
       </div>
     );
   }
 }
 
 export default App;
+
+function getTubeLines() {
+  var tubeStations = [];
+
+  fetch("https://api.tfl.gov.uk/Line/Mode/tube")
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.forEach(function(station){
+          var simoplifiedStation = {
+            id: station.id,
+            name: station.name
+          };
+          tubeStations.push(simoplifiedStation);
+        })
+          // console.log(JSON.stringify(tubeStations));
+          return tubeStations;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+}
