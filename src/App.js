ba24c8d22f5d0dfd8d34b25652484bf7124c0b4a
Tim Harrison <12405008+trwh@users.noreply.github.com>
2019-02-13 15:11:58 +0000

    Refactor promises to ES6 arrow notation.

--- src/App.js
+++ src/App.js
@@ -11,18 +11,12 @@ class App extends Component {
 
   render() {
     getTubeLines()
-      .then(tubeLines => {
-        console.log("Tube lines:");
-        console.log(JSON.stringify(tubeLines));
-        return tubeLines;
-      })
       .then(tubeLines => getTubeStations(tubeLines))
       .then(tubeStations => {
         console.log("Tube stations:");
         console.log(JSON.stringify(tubeStations));
       })
-      .catch(err => console.log(err)
-      );
+      .catch(err => console.log(err));
 
     return (
       <div>
@@ -34,7 +28,6 @@ class App extends Component {
 
 export default App;
 
-// TODO: Refactor for arrow functions
 function getTubeLines() {
   return new Promise(
     function(resolve, reject) {
@@ -42,14 +35,14 @@ function getTubeLines() {
       var tubeLines = [];
 
       fetch("https://api.tfl.gov.uk/line/mode/tube")
-        .then(function(response) {
+        .then(response => {
           if(response.ok) {
             return response.json();
           } else {
             throw new Error("Error getting list of tube lines from TFL API.")
           }
         })
-        .then(function(json) {
+        .then(json => {
           json.forEach(function(line){
             var simplifiedLine = {
               id: line.id,
@@ -59,9 +52,7 @@ function getTubeLines() {
           })
           resolve(tubeLines);
         })
-        .catch(function(err) {
-          reject(err);
-        });
+        .catch(err => reject(err));
 
     }
   )
@@ -73,53 +64,35 @@ function getTubeStations(tubeLines) {
 
       var tubeStations = [];
       var tubeStationFetchPromises = [];
-      // var tubeStationFetchResponseJsons = [];
 
       tubeLines.forEach(line => {
+
         tubeStationFetchPromises.push(
           fetch("https://api.tfl.gov.uk/line/" + line.id + "/stoppoints")
-            .then(response => response.json())
+            .then(response => {
+              if(response.ok) {
+                return response.json();
+              } else {
+                throw new Error("Error getting list of tube stations from TFL API.")
+              }
+            })
         );
-      })
 
-      // console.log("Just made the array of promises to resolve all. Here it is: "
-      //   + JSON.stringify(tubeStationFetchPromises)
-      // );
+      })
 
       Promise.all(tubeStationFetchPromises)
         .then(jsonResponses => {
 
-          // console.log("We got this many fetch responses: " +
-          //   responses.length);
-          // console.log("Here is the first fetch response: " +
-          //   JSON.stringify(responses[0]));
-
-          // responses.forEach(response => {
-          //   console.log("Dealing with a response now.");
-          //   if (response.ok) {
-          //     tubeStationFetchResponseJsons.push(response.json());
-          //   } else {
-          //     throw new Error("Error getting list of tube stations from TFL API.")
-          //   }
-          // })
-
-          console.log("OK, now we have " +
-            jsonResponses.length +
-            " response JSON objects to work with. Here they all are: "
-            + JSON.stringify(jsonResponses)
-            );
-
           jsonResponses.forEach(json => {
-            // console.log("Dealing with a response JSON object now. Here's one of many: "
-              // + JSON.stringify(json));
+
             Array.from(json).forEach(station => {
-              // console.log("Found a station object!");
               var simplifiedStation = {
                 id: station.id,
                 name: station.commonName,
               };
               tubeStations.push(simplifiedStation);
             })
+
           })
 
           console.log("OK, there were " + tubeStations.length
