a172a879ed3ec979248cd2fe3ebb093212fdfa28
Tim Harrison <12405008+trwh@users.noreply.github.com>
2019-02-14 11:30:24 +0000

    Begin building station picker, based on reactjs.org to do list demo.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Cookies from 'universal-cookie';
-import './App.css';
+// import './App.css';
 
 const tubeStations = require("./stations.json");
 const cookies = new Cookies();
@@ -9,29 +9,98 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    console.log(JSON.stringify(tubeStations));
+    // getTubeLines()
+    //   .then(tubeLines => getTubeStations(tubeLines))
+    //   .then(tubeStations => console.log("Tube stations from API: " + JSON.stringify(tubeStations)))
+    //   .catch(err => console.log(err));
 
-    // TODO: Store tubeStations in state
     this.state = {
-      myStation: ""
+      userTubeStations: [],
+      inputText: ""
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ inputText: e.target.value });
+  }
+
+  // TODO: Get ID of station submitted?
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.inputText.length) {
+      return;
+    }
+    const newStation = {
+      name: this.state.inputText,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      userTubeStations: state.userTubeStations.concat(newStation),
+      inputText: ""
+    }));
   }
 
   render() {
-    // getTubeLines()
-    //   .then(tubeLines => getTubeStations(tubeLines))
-    //   .then(tubeStations => console.log("Tube stations from API: " + tubeStations.length))
-    //   .catch(err => console.log(err));
-
     return (
       <div>
-        Hello! Let's look for {this.state.station}!
+        <h3>Set local station(s)</h3>
+        <StationList stations={this.state.userTubeStations} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-station">
+            Station name:
+          </label>
+          <input
+            id="new-station"
+            onChange={this.handleChange}
+            value={this.state.inputText}
+          />
+          <button>
+            Add station
+          </button>
+        </form>
       </div>
     );
   }
 }
 
+class StationList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.stations.map(station => (
+          <li key={station.id}>{station.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
 export default App;
+
+// TODO: This becomes code to store our list of set stations.
+// function getLocalStationsFromCookies() {
+//   if (cookies.get("tfl-arr")) {
+//     console.log("Hey, there's a cookie!");
+//     return JSON.parse(cookies.get("tfl-arr"));
+//   } else {
+//     console.log("No cookie set, getting stations manually.");
+//     getTubeLines()
+//       .then(tubeLines => getTubeStations(tubeLines))
+//       .then(tubeStations => {
+//         console.log("Tube stations from API: " + tubeStations.length);
+//         cookies.set("tfl-arr", JSON.stringify(tubeStations),
+//           {
+//             path: "/",
+//             maxAge: 31536000
+//           }
+//         );
+//         return tubeStations;
+//       })
+//       .catch(err => console.log(err));
+//   }
+// }
 
 function getTubeLines() {
   return new Promise(
